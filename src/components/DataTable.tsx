a3828194fb34736e5da1f5736c7ac7b0a6977453
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-06-07 15:25:44 -0400

    adding phonebook-VIII

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Button from "./Button"
 import Modal from "./Modal"
+import { server_calls } from '../api/server';
 
 
 function DataTable() {
@@ -14,11 +15,17 @@ function DataTable() {
         setOpen(false)
     }
 
+    const getData = async () => {
+        const result = await server_calls.get();
+        console.log(result)
+    }
+
 
   return (
     <>
         <Modal 
             open={open}
+            onClose={handleClose}
         />
         <div className="flex flex-row">
             <div>
@@ -29,11 +36,11 @@ function DataTable() {
                     Create New Contact
                 </button>
             </div> 
-            <button onClick={() => handleClose()}> close</button>
             <Button className="p-3 bg-slate-300 rounded m-3 hover:bg-slate-800 hover:text-white" >Update</Button>
             <Button className="p-3 bg-slate-300 rounded m-3 hover:bg-slate-800 hover:text-white" >Delete</Button>
         </div>
         {/* Data Table section */}
+        <button onClick={getData}>get Data</button>
     </>
   )
 }
