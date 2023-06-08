813191296013dbfc30a26831a5135f38880d9508
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-06-08 17:07:01 -0400

    finished phonebook-XI

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,24 +1,39 @@
+import { useSubmit } from "react-router-dom"
 import Button from "./Button"
 import Input from "./Input"
-const ContactForm = () => {
+
+import { useForm } from 'react-hook-form'
+
+interface ContactFormProps {
+  id?: string,
+  data?: {}
+}
+
+const ContactForm = ( props:ContactFormProps) => {
+  const { register, handleSubmit } = useForm({})
+
+  const onSubmit = () => {
+    console.log('pass')
+  }
+
   return (
     <div>
-      <form onSubmit={() => console.log('submitted')}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="name">Contact Name</label>
-          <Input name='name' placeholder="Name" />
+          <Input {...register('name')} name='name' placeholder="Name" />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <Input name='email' placeholder="Email" />
+          <Input {...register('email')} name='email' placeholder="Email" />
         </div>
         <div>
           <label htmlFor="phone_number">Phone Number</label>
-          <Input name='phone_number' placeholder="Phone Number" />
+          <Input {...register('phone_number')} name='phone_number' placeholder="Phone Number" />
         </div>
         <div>
           <label htmlFor="address">Address</label>
-          <Input name='address' placeholder="Address" />
+          <Input {...register('address')} name='address' placeholder="Address" />
         </div>
         <div className="flex p-1">
           <Button className="flex justify-start m-3 bg-slate-300 p-2 rounded hover:bg-slate-800 text-white"
---
+++ test.py
@@ -0,0 +1,4 @@
+nums = ['a','b','c','d']
+
+for num in nums:
+    print(num)
