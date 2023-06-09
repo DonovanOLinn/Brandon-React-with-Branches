45191020df0c62f60c18e66c43e4b920844e6d6b
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-06-09 15:11:30 -0400

    phonebook XIV

--- src/api/server.ts
+++ src/api/server.ts
@@ -12,5 +12,56 @@ export const server_calls = {
 
         return await response.json()
     },
+
+    create: async (data: any = {}) => {
+        const response = await fetch(`https://plankton-app-46k8b.ondigitalocean.app/api/contacts`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to create new data on the server')
+        }
+
+        return await response.json()
+    },
+
+    update: async (id:string, data: any = {}) => {
+        const response = await fetch(`https://plankton-app-46k8b.ondigitalocean.app/api/contacts/${id}`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to update data on server')
+        }
+
+        return await response.json()
+    },
+
+    delete: async (id:string) => {
+        const response = await fetch(`https://plankton-app-46k8b.ondigitalocean.app/api/contacts/${id}`,
+        {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to delete data on server')
+        }
+
+        return;
+    },
     
 }
