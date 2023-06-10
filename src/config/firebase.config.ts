4b3f661d33901d455e93628fe01e070bab145fc7
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-06-10 15:14:00 -0400

    finished phonebook XVIII

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAHSB7o8e3OzwjOFn-ogRE_7fDCO_OJaLc",
+    authDomain: "sp-fs-react-update-v18-1e70b.firebaseapp.com",
+    projectId: "sp-fs-react-update-v18-1e70b",
+    storageBucket: "sp-fs-react-update-v18-1e70b.appspot.com",
+    messagingSenderId: "258560947763",
+    appId: "1:258560947763:web:74df8728d617bf2d469546"
+  };
