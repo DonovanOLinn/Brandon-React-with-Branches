9ee1d46a4f6917d7cf7ef2b6ba4f8bdd0f86d2bc
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-06-10 15:20:10 -0400

    finishing Hosting touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter, Routes, Route, Link } from 'react-router-dom'
 import routes from './config/routes'
 import Navbar from './components/Navbar'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
 import AuthChecker from './auth/AuthChecker'
-
+//NOTES PLEASE LOOK HERE
+// So, if you hosted to Netlify, and you are getting errors pop up whenever you try to add, update, or delete from your dashboard,
+// switch out all of the BrowserRouters on this page to HashRouters. That has fixed the problem. If you manage to find another solution,
+// please reach out and let me know!
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Provider store={store}>
         <Routes>
@@ -31,7 +34,7 @@ function App() {
           )) }
         </Routes>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
